ca52599ae6e0bd3b3f83b749446cd0cf816b8831
grey <marc.hesse@gmail.com>
2019-12-16 05:19:27 -0500

    basic react setup

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const {dirList} = require('./dirList');
 
 const app = express();
 const port = 8080;
-const staticDir = path.resolve('public');
+const filesDir = path.resolve('files');
+const staticDir = path.resolve('dist');
 
-app.use('/files', dirList(staticDir));
-app.use('/files', express.static(staticDir, {index: false}));
+app.use('/files', dirList(filesDir));
+app.use('/files', express.static(filesDir, {index: false}));
+app.use('/', express.static(staticDir));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'frontend.js',
+    libraryTarget: 'umd'
+  },
+
+  /*
+  devServer: {
+    contentBase: path.resolve(__dirname),
+    compress: true,
+    port: 9000,
+    host: 'localhost',
+    open: true
+  },
+  */
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+}
